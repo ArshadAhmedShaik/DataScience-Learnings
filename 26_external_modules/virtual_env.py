77a8f61b9cd776ff3a109229ceec30ad79fd826b
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-30 12:57:03 +0530

    Added today's learnings

---
+++ 26_external_modules/virtual_env.py
@@ -0,0 +1,22 @@
+# v1 - package 1 (pandas)
+# v1 - package 2 (numpy)
+# v3 - package 3 (moviepy)
+# we have to active the env1
+# virtual envi -> fresh start with fresh packages
+
+# dependencies means all tools and modules required
+# dependency of my project
+# requirements.txt
+
+# deactive -> deactivates the virtual environment
+
+# to activate env2 -> ".\env2\Scripts\Activate.ps1"
+
+import moviepy
+
+
+
+
+
+
+
